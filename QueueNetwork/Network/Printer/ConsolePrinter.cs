34ff4c5e643f482e07b5461f431b2f25af327263
Hidde Wieringa <hidde@hiddewieringa.nl>
2015-06-27 17:28:56 +0200

    Issue #1. Started on printers

---
+++ QueueNetwork/Network/Printer/ConsolePrinter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace QueueNetwork.Printer {
+	public class FilePrinter : INetworkPrinter {
+		public FilePrinter (String location) {
+		}
+
+		public void PrintNetwork(Network network) {
+			throw new NotImplementedException ();
+		}
+	}
+}
+
---
+++ QueueNetwork/Network/Printer/FilePrinter.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace QueueNetwork {
+	public class FilePrinter {
+		public FilePrinter () {
+		}
+	}
+}
+
