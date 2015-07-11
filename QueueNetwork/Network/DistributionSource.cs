7e7895d3288ad8851e642e2c436413e4c381c6ab
Hidde Wieringa <hidde@hiddewieringa.nl>
2015-07-11 16:49:49 +0200

    Added DistributionSource

---
+++ QueueNetwork/Network/DistributionSource.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+using QueueNetwork.Distibution;
+
+namespace QueueNetwork {
+	public class DistributionSource : Source {
+		private IDistribution<double> Distribution;
+		private double nextDeparture;
+
+		public DistributionSource (IDistribution<double> distribution) {
+			Distribution = distribution;
+			nextDeparture = Clock.GetTime() + Distribution.NextRandom ();
+		}
+
+		public override Dictionary<Trigger, double> NextTriggers() {
+			return new Dictionary<Trigger, double> {
+				{new DepartTrigger(this), nextDeparture}
+			};
+		}
+
+		public override void Trigger (Trigger t) {
+			if (t is DepartTrigger) {
+				CallPreEvent (new DepartEvent (this, DepartLocation));
+				nextDeparture = Clock.GetTime() + Distribution.NextRandom ();
+
+				Unit unit = new Unit ();
+				unit.SystemArriveTime = Clock.GetTime ();
+				unit.Source = this;
+
+				CallPostEvent (new DepartEvent (this, DepartLocation));
+				DepartLocation.Arrive (unit, this);
+
+				return;
+			}
+
+			throw new UnknownEventException ();
+		}
+	}
+}
+
--- QueueNetwork/Network/PoissonSource.cs
+++ QueueNetwork/Network/PoissonSource.cs
@@ -4,37 +4,8 @@ using System.Collections.Generic;
 using QueueNetwork.Distibution;
 
 namespace QueueNetwork {
-	public class PoissonSource : Source {
-		private ExponentialDistribution Distribution;
-		private double nextDeparture;
-
-		public PoissonSource (double lambda) {
-			Distribution = new ExponentialDistribution(lambda);
-			nextDeparture = Clock.GetTime() + Distribution.NextRandom ();
-		}
-
-		public override Dictionary<Trigger, double> NextTriggers() {
-			return new Dictionary<Trigger, double> {
-				{new DepartTrigger(this), nextDeparture}
-			};
-		}
-
-		public override void Trigger (Trigger t) {
-			if (t is DepartTrigger) {
-				CallPreEvent (new DepartEvent (this, DepartLocation));
-				nextDeparture = Clock.GetTime() + Distribution.NextRandom ();
-
-				Unit unit = new Unit ();
-				unit.SystemArriveTime = Clock.GetTime ();
-				unit.Source = this;
-
-				CallPostEvent (new DepartEvent (this, DepartLocation));
-				DepartLocation.Arrive (unit, this);
-
-				return;
-			}
-
-			throw new UnknownEventException ();
+	public class PoissonSource : DistributionSource {
+		public PoissonSource (double lambda) : base (new ExponentialDistribution(lambda)) {
 		}
 	}
 }
